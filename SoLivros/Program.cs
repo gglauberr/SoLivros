53f14ab6cdc0d5dcd72eb08d56d2853a851277bd
gglauberr <glauberpp16@yahoo.com.br>
2020-12-22 03:48:42 -0300

    Ajustes

--- SoLivros/Program.cs
+++ SoLivros/Program.cs
@@ -1,13 +1,29 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading.Tasks;
 
 namespace SoLivros.API
 {
+    using SoLivros.DataAccess.EF;
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var annotPadContext = services.GetService<SoLivrosContext>();
+                annotPadContext.Database.SetCommandTimeout((int)TimeSpan.FromMinutes(30).TotalSeconds);
+
+                await annotPadContext.Database.MigrateAsync();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
